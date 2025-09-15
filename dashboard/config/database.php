010cb1e215619ec3f6c9a45fedf45653dc17bb93
Adi Fayyaz <adisumardi888@gmail.com>
2025-09-15 11:34:09 +0700

    mengganti default redis 3

--- dashboard/config/database.php
+++ dashboard/config/database.php
@@ -35,19 +35,24 @@ return [
 
             'client' => env('REDIS_CLIENT', 'phpredis'),
 
-            'default' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', 6379),
-                'database' => (int) env('REDIS_DB', 0),
-            ],
+                'options' => [
+                    'cluster' => env('REDIS_CLUSTER', 'redis'),
+                    'prefix' => env('REDIS_PREFIX', ''),
+                ],
 
-            'cache' => [
-                'host' => env('REDIS_HOST', '127.0.0.1'),
-                'password' => env('REDIS_PASSWORD', null),
-                'port' => env('REDIS_PORT', 6379),
-                'database' => (int) env('REDIS_CACHE_DB', 1),
-            ],
+                'default' => [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => (int) env('REDIS_DB', 0),
+                ],
+
+                'cache' => [
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'password' => env('REDIS_PASSWORD'),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => (int) env('REDIS_CACHE_DB', 0),
+                ],
         ],
 
         'sqlite' => [
